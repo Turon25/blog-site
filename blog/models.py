fa58db6c012f5c932ae0f90d024d4cecb494cb90
Temurbek <temurbekimomaliyev7@gmail.com>
2025-03-29 16:31:34 +0500

    git commit -m "initial project"

--- blog/models.py
+++ blog/models.py
@@ -1,34 +1,51 @@
+from django.db.models import CharField
+from django.urls import reverse
 from django.utils import timezone
 from django.db import models
-from django.utils.text import slugify
-
+# from django.utils.text import slugify
+from django.contrib.auth.models import User
 
 # Create your models here.
-class Author(models.Model):
-    name = models.CharField(max_length=100, blank=True, null=True)
-    age = models.SmallIntegerField()
-    link = models.URLField(null=True)
+class PublishedManager(models.Manager):
 
-    def __str__(self):
-        return self.name
+    def get_queryset(self):
+        return super(PublishedManager, self).get_queryset().filter(status='published')
 
 
 class Post(models.Model):
+    STATUS_CHOICES = (
+        ( 'draft', 'Draft'),
+        ('published', 'Published')
+    )
     title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=255, unique=True, blank=True, null=True)
-    content = models.TextField()
-    author = models.ForeignKey(Author, null=True, on_delete=models.CASCADE)
+    slug = models.SlugField(max_length=255, unique_for_date="publish ")
+    body = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_posts")
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
+
+    class Meta:
+        ordering = ('-publish',)
+
 
     def __str__(self):
         return self.title
 
-    def save(self, *args, **kwargs):
-        if not self.slug:
-            self.slug = slugify(self.title)
-        super().save(*args, **kwargs)
+    objects = models.Manager()
+    published = PublishedManager()
+
+
+    # def get_absolute_url(self):
+        # return reverse("")
+
+
+post = Post.objects.filter(status='published')
+p_posts = Post.published.all()
+
+
+
 
 
 
